082223c9ee10a129d9ac1af420c9a04dece66618
Rodgy123 <rodgowengraham88@gmail.com>
2023-02-16 10:01:04 -0500

    idk

---
+++ Topic 5 - Decision Structures/Part 5.0 Decision Structures/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Part_5._0_Decision_Structures
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
