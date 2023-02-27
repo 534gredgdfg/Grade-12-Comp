472752714f2990a7b5acf4a4bdcd4a7ecd69a6d7
534gredgdfg <owenrodgers45@gmail.com>
2023-02-27 11:01:24 -0500

    started

---
+++ Dice Game/Dice Game/Program.cs
@@ -0,0 +1,79 @@
+﻿namespace Dice_Game
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Double Balence;
+            int betType, sum;
+            Balence = 100.00;
+            Die rolling = new Die();
+            
+            Console.WriteLine($"You initial balence is {Balence}");
+            Console.WriteLine("Would you like to bet on 1.double, 2.not doubles, 3.even sum or 4.odd sum");
+            Console.WriteLine("");
+            
+
+            if (Int32.TryParse(Console.ReadLine(), out betType))
+            {
+
+                if (betType == 1)
+                {
+                    Console.WriteLine("");
+                    Console.WriteLine("You bet on doubles");
+                    Rolling_the_dice();
+                }
+                else if (betType == 2)
+                {
+                    Console.WriteLine("");
+                    Console.WriteLine("You bet on not doubles");
+                    Rolling_the_dice();
+                }
+                else if (betType == 3)
+                {
+                    Console.WriteLine("");
+                    Console.WriteLine("You bet on even sum");
+                    Rolling_the_dice();
+                    
+                    
+
+                }
+                else if (betType == 4)
+                {
+                    Console.WriteLine("");
+                    Console.WriteLine("You bet on odd sum");
+                    Rolling_the_dice();
+                }
+                else
+                {
+                    Console.WriteLine("Invalid Input");
+                }
+
+            }
+            else
+            {
+                Console.WriteLine("Invalid Input");
+            }
+
+
+        } 
+        public static void Rolling_the_dice()
+        {
+            Die rolling = new Die();
+            Console.WriteLine("");
+            int roll1;
+            int roll2;
+
+
+            Console.WriteLine($"You rolled a {rolling}");           
+            rolling.DrawRoll();
+            roll1 = rolling.Roll;
+            
+
+            rolling.RollDie();
+            Console.WriteLine($"And rolled a {rolling}");
+            rolling.DrawRoll();
+            roll2 = rolling.Roll;
+        }
+    }
+}
---
+++ Part 6 Looping/Part 6-Looping Problems/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Part_6_Looping_Problems
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
